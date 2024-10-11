fac9cf15e0cd5046a71393588fc56acdd389fe56
The-Professor99 <festusihechi99@gmail.com>
2024-10-11 21:04:41 +0100

    style: add download-app component styling

--- src/components/download-app.tsx
+++ src/components/download-app.tsx
@@ -1,30 +1,61 @@
 import phoneImage from "assets/phone.png";
 import AppleLogo from "assets/apple_logo.svg";
 import PlaystoreLogo from "assets/playstore_logo.svg";
+import Typography from "./atoms/Typography";
+import Button from "./atoms/Buttons";
+import Image from "./atoms/Image";
+import Container from "./atoms/Container";
+import "./download-app.scss";
 
 export default function DownloadApp() {
   return (
-    <section>
-      <div>
-        <p>Download The Rise App</p>
-        <h2>Join our 100,000 users investing and setting long term goals!</h2>
-        <p>Dollar investments that help you grow.</p>
+    <Container centerContent>
+      <section className="download--app rounded-3xl p-sm">
         <div>
-          <button>
-            <span>
-              <img src={AppleLogo} alt="apple phone" />
-            </span>
-            Download on the App Store
-          </button>
-          <button>
-            <img src={PlaystoreLogo} alt="playstore" />
-            Download on the Google Play
-          </button>
+          <Typography variant="h2" color="primary" className="mt-lg mb-md">
+            Download The Rise App
+          </Typography>
+          <Typography variant="h3" weight="bold">
+            Join our 100,000 users investing and setting long term goals!
+          </Typography>
+
+          <Typography weight="bold" className="mt-md mb-lg">
+            Dollar investments that help you grow.
+          </Typography>
+
+          <div className="flex mt-lg mb-lg gap-3 ">
+            <Button>
+              <div>
+                <img src={AppleLogo} alt="apple phone" />
+                <Typography weight="light">
+                  Download on the
+                  <br />
+                  <Typography variant="span" weight="bold">
+                    App Store
+                  </Typography>
+                </Typography>
+              </div>
+            </Button>
+
+            <Button>
+              <div>
+                <img src={PlaystoreLogo} alt="playstore" />
+
+                <Typography weight="light">
+                  Download on the
+                  <br />
+                  <Typography variant="span" weight="bold">
+                    Google Play
+                  </Typography>
+                </Typography>
+              </div>
+            </Button>
+          </div>
         </div>
-      </div>
-      <div>
-        <img src={phoneImage} alt="Risevest app display on a phone" />
-      </div>
-    </section>
+        <div>
+          <Image src={phoneImage} alt="Risevest app display on a phone" />
+        </div>
+      </section>
+    </Container>
   );
 }
