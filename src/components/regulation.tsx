a6a81c2461a7487081bfdb0bd91b3b71d588a9dc
The-Professor99 <festusihechi99@gmail.com>
2024-10-11 23:40:12 +0100

    refactor: use typography component for regulation texts

--- src/components/regulation.tsx
+++ src/components/regulation.tsx
@@ -1,13 +1,39 @@
+import Container from "./atoms/Container";
+import Typography from "./atoms/Typography";
+import "./regulation.scss";
+
 export default function Regulation() {
   return (
-    <article>
-      <h2>How we are Regulated</h2>
-      <p>
-        Rise is registered and regulated both in the US and in Nigeria. Our team
-        is made up of US registered investment advisers, our Nigerian users are
-        covered by our SEC licensed trustees, ARM Trustees and all our assets
-        are held with regulated third parties, in all relevant jurisdictions
-      </p>
-    </article>
+    <div className="regulation--container">
+      <article className="regulation">
+        <Container centerContent>
+          <div>
+            <Typography
+              variant="h2"
+              weight="bold"
+              size="large"
+              className="m-md"
+            >
+              How we are Regulated
+            </Typography>
+            {/* <Typography>
+            Rise is registered and regulated both in the US and in Nigeria. Our
+            team is made up of US registered investment advisers, our Nigerian
+            users are covered by our SEC licensed trustees, ARM Trustees and all
+            our assets are held with regulated third parties, in all relevant
+            jurisdictions
+          </Typography> */}
+            <Typography>
+              Rise is registered and regulated both in the US and in Nigeria.
+              The founder is registered as an investment adviser with the US
+              SEC. The Nigerian SEC regulates Rise's Nigerian investments
+              through a trusteeship agreement with ARM Trustees, who oversees
+              investments on behalf of the users. And a registered investment
+              subsidiary holds all users' assets regulated in the US
+            </Typography>
+          </div>
+        </Container>
+      </article>
+    </div>
   );
 }
