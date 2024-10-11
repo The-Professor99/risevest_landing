9f89472d23a5d554749ace113e9638db6204880f
The-Professor99 <festusihechi99@gmail.com>
2024-10-11 23:38:37 +0100

    style: add partners component styles

--- src/components/partners.tsx
+++ src/components/partners.tsx
@@ -2,17 +2,65 @@ import armLogo from "assets/arm.png";
 import westernUnionLogo from "assets/western_union.png";
 import techstarsLogo from "assets/techstars.jpg";
 import venturesLogo from "assets/ventures_platform.png";
+import Container from "./atoms/Container";
+import Image from "./atoms/Image";
+import React from "react";
+
+const Partner = ({
+  imgSrc,
+  imgAlt,
+  imgLink,
+}: {
+  imgSrc: string;
+  imgAlt: string;
+  imgLink: string;
+}) => {
+  return (
+    <div>
+      <a href={imgLink}>
+        <Image
+          src={imgSrc}
+          alt={imgAlt}
+          className="w-full h-full object-contain "
+        />
+      </a>
+    </div>
+  );
+};
 
 export default function Partners() {
+  const partners = [
+    { id: 1, imgSrc: armLogo, imgAlt: "ARM Logo", imgLink: "#" },
+    {
+      id: 2,
+      imgSrc: westernUnionLogo,
+      imgAlt: "Western Union logo",
+      imgLink: "#",
+    },
+    { id: 3, imgSrc: techstarsLogo, imgAlt: "Techstars logo", imgLink: "#" },
+    {
+      id: 4,
+      imgSrc: venturesLogo,
+      imgAlt: "Ventures Platform logo",
+      imgLink: "#",
+    },
+  ];
   return (
-    <article>
-      <h2>We are supported by</h2>
-      <div>
-        <img src={armLogo} alt="ARM logo" />
-        <img src={westernUnionLogo} alt="Western Union logo" />
-        <img src={techstarsLogo} alt="Techstars logo" />
-        <img src={venturesLogo} alt="Ventures Platform logo" />
-      </div>
-    </article>
+    <Container>
+      <article>
+        <h2>We are supported by</h2>
+        <div className="flex h-16">
+          {partners.map((partner) => (
+            <React.Fragment key={partner.id}>
+              <Partner
+                imgAlt={partner.imgAlt}
+                imgLink={partner.imgLink}
+                imgSrc={partner.imgSrc}
+              />
+            </React.Fragment>
+          ))}
+        </div>
+      </article>
+    </Container>
   );
 }
